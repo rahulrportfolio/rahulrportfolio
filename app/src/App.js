1854a090337aed052d746d0e04a4c23c6d403b89
Rahul Ramjeawon <96823012+CodeWithTejpratab@users.noreply.github.com>
2024-04-18 09:05:45 -0400

    current

---
+++ app/src/App.js
@@ -0,0 +1,21 @@
+import './App.css';
+import {Project, Bio, Home, NavBar, Contact} from './components';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
+function App() {
+  return (
+    <div className="App">
+      <Router basename="/rahulrportfolio">
+        <NavBar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/bio" element={<Bio />} />
+          <Route path="/project" element={<Project />} />
+        </Routes>
+    </Router>
+    <Contact />
+    </div>
+  );
+}
+
+export default App;
