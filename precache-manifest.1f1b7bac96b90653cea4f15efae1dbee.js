890a6e6338f1c621d0058a3d1901778cd087f824
Rahul Ramjeawon <96823012+CodeWithTejpratab@users.noreply.github.com>
2024-06-09 04:07:09 -0400

    Updates

---
+++ precache-manifest.1f1b7bac96b90653cea4f15efae1dbee.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba71abe41d8a50dae557ba499d762a62",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a96ef68bb643600a1588",
+    "url": "/static/css/main.3dd59fc1.chunk.css"
+  },
+  {
+    "revision": "ad39b97e84d2b5019e86",
+    "url": "/static/js/2.208cf289.chunk.js"
+  },
+  {
+    "revision": "70c068b2fb9d959ad245",
+    "url": "/static/js/3.730e5367.chunk.js"
+  },
+  {
+    "revision": "a96ef68bb643600a1588",
+    "url": "/static/js/main.2ef3f31c.chunk.js"
+  },
+  {
+    "revision": "ffe5ef1c22255a83fb38",
+    "url": "/static/js/runtime~main.f8c8db13.js"
+  },
+  {
+    "revision": "f35b032f8e2d33b155ed9c3f6f91c3c3",
+    "url": "/static/media/github.f35b032f.png"
+  },
+  {
+    "revision": "2c1a7560c88ea83e6b2593cd07af8ad8",
+    "url": "/static/media/gmail.2c1a7560.png"
+  },
+  {
+    "revision": "fd0d5546fdbdc85c76c4372a0d51f1bc",
+    "url": "/static/media/linkedin.fd0d5546.png"
+  },
+  {
+    "revision": "47aac94bd08be097d2fb170df4c75211",
+    "url": "/static/media/me.47aac94b.png"
+  }
+]);
